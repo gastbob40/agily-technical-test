74df60abc0bc1311180e8fe6f0173586a39e5006
Quentin Briolant <quentin.briolant@epita.fr>
2022-10-28 15:50:37 +0200

    feat(home): add basic home page
    Signed-off-by: Quentin Briolant <quentin.briolant@epita.fr>

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/scss/reset.scss',
+    '@/assets/scss/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <h1>The Forecast<br>Weather APP</h1>
+
+    <form action="/" method="get">
+      <input type="search" placeholder="Search">
+      <button type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path fill="currentColor"
+                d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"
+          ></path>
+        </svg>
+      </button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +22,69 @@ export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
+
+<style lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 64px;
+
+  height: 100vh;
+  width: 100vw;
+
+  padding: 16px;
+}
+
+h1 {
+  font-size: 3em;
+  line-height: 1em;
+  font-weight: 900;
+
+  background: -webkit-linear-gradient(#25c35d, #d8e69b);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+form {
+  position: relative;
+  width: 100%;
+  max-width: 500px;
+}
+
+input {
+  width: 100%;
+  height: 60px;
+
+  color: var(--accent-color);
+  font-size: 1.1em;
+  font-weight: 700;
+
+  border-radius: 15px;
+  outline: none;
+  border: none;
+  padding: 0 60px 0 20px;
+}
+
+
+button {
+  position: absolute;
+  padding: 5px 0 0 0;
+  cursor: pointer;
+
+  top: 50%;
+  right: 15px;
+
+  transform: translateY(-50%);
+
+  background: transparent;
+  border: none;
+  outline: none;
+
+  & svg {
+    width: 30px;
+    height: 30px;
+  }
+}
+</style>
